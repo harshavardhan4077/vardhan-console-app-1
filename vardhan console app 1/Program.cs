57071ffe6aae08aa852c660c3fa8fd0625f8839e
Harshavardhan M <Harshavardhan M@LAPTOP-UAUHK2T4>
2021-07-25 17:10:54 +0530

    Add project files.

---
+++ vardhan console app 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vardhan_console_app_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("my first project sir");
+            Console.WriteLine("please support me");
+            Console.WriteLine("HARSHAVARDHAN.M");
+            Console.ReadKey();
+        }
+    }
+}
